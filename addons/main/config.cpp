5309318f992b68ed723b353a3179da2ef5ca15f1
applechaser <appelchaser@gmail.com>
2025-12-05 15:06:59 +0100

    add simple wardrobe config, seperate cfgWeapons

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -9,28 +9,5 @@ class cfgPatches {
     };
 };
 
-
-class CfgWeapons
-{
-    class arifle_MX_Base_F;
-    class arifle_MX_F: arifle_MX_Base_F
-    {
-        magazineWell[] = {"MX_65x39","CBA_65x39_MX"};
-    };
-    class arifle_MX_GL_F: arifle_MX_Base_F
-    {
-        magazineWell[] = {"MX_65x39","CBA_65x39_MX"};
-    };
-    class arifle_MXC_F: arifle_MX_Base_F
-    {
-        magazineWell[] = {"MX_65x39","CBA_65x39_MX"};
-    };
-    class arifle_MXM_F: arifle_MX_Base_F
-    {
-        magazineWell[] = {"MX_65x39","CBA_65x39_MX"};
-    };
-    class ef_arifle_mxar: arifle_MX_Base_F
-    {
-        magazineWell[] = {"MX_65x39","CBA_65x39_MX"};
-    };
-};
+#include "CfgWeapons.hpp"
+#include "ace_wardrobe.hpp"
--- addons/main/script_component.hpp
+++ addons/main/script_component.hpp
@@ -2,6 +2,7 @@
 #define COMPONENT main
 
 #include "\x\cba\addons\main\script_macros_common.hpp"
+#include "\z\ace\addons\wardrobe\script_macros_wardrobe.hpp"
 
 #undef PREP
 #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
